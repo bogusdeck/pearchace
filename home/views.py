626817f1751363cad635ea6fe114defca93b4124
bogusdeck <tanishvashisth@gmail.com>
2024-09-15 20:29:57 +0530

    jwt setup done

--- home/views.py
+++ home/views.py
@@ -46,6 +46,11 @@ from django.http import JsonResponse
 from django.shortcuts import redirect
 import pytz
 
+from django.shortcuts import redirect
+from django.http import JsonResponse
+from rest_framework_simplejwt.tokens import RefreshToken
+from datetime import datetime
+import pytz
 
 @shop_login_required
 def index(request):
@@ -107,14 +112,14 @@ def index(request):
         access_token = str(refresh.access_token)
         refresh_token = str(refresh)
 
-        frontend_url = f"https://pearch.vercel.app/"
-        redirect_url = f"{frontend_url}?access_token={access_token}&refresh_token={refresh_token}"
+        frontend_url = "https://pearch.vercel.app/"
+        redirect_url = f"{frontend_url}?access_token={access_token}&refresh_token={refresh_token}&shop_url={shop_url}"
 
         return redirect(redirect_url)
 
     except Exception as e:
         return JsonResponse({'error': str(e)}, status=500)
-        
+
 
 @api_view(['GET'])
 @permission_classes([IsAuthenticated])
