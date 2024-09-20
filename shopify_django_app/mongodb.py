264309f09ecbaae04c62af7a85e746b7cb5f9ac0
bogusdeck <tanishvashisth@gmail.com>
2024-09-20 23:12:13 +0530

    auth in faqs and status api

--- shopify_django_app/mongodb.py
+++ shopify_django_app/mongodb.py
@@ -5,6 +5,7 @@ from django.conf import settings
 from django.http import JsonResponse
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated
+from rest_framework_simplejwt.authentication import JWTAuthentication
 
 @api_view(['GET'])
 @permission_classes([IsAuthenticated])
